965a41eb8251daab5f042315069f0151d93388a5
Miguel Sanchez <migsandia@gmail.com>
2019-02-13 14:05:51 +0100

    tenemos la pagina principal

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 'use strict'
+
+const main = () => {
+  const buildDom = (html) =>{
+    const main = document.querySelector('main');
+    main.innerHTML = html;
+    return main;
+  };
+
+  const buildSplashScreen = () => {
+    const  splashScreen = buildDom(`
+    <section class="splash_screen"></section>
+    <h1>Eternal Enemies</h1>
+    <button>Start</button>
+    `);
+    
+  };
+  buildSplashScreen();
+};
+
+window.addEventListener('load',main);
